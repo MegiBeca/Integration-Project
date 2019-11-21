c17edf6b8076252f262439cae5fa906e5aca2100
MegiBeca <54412551+MegiBeca@users.noreply.github.com>
2019-11-21 18:09:27 -0500

    Update and rename Sales to Sales.py

--- Sales.py
+++ Sales.py
@@ -6,7 +6,6 @@ author = Megi Beca
 
 # Calculates the percent saved off an item then format's the output
 def identifying_percent_off():
-    print("Hello")
     original_price = float(input("Enter the original cost of the item: "))
     sale_price = float(input("Enter the sale price: "))
     percent_off = int((original_price - sale_price) / original_price * 100)
@@ -25,10 +24,35 @@ def identifying_savings():
             is_bad = False
             percent_off = int((original_price - price) / original_price * 100)
             if percent_off >= 70:
-                print("You got", percent_off, "percent off, you got a great deal")
+                print("You got", percent_off, "percent off, you got a great deal.")
             elif percent_off >= 50:
-                print("You got", percent_off, "percent off, you got a deal")
+                print("You got", percent_off, "percent off, you got a deal.")
             else:
-                print("You got", percent_off, "percent off, you got an ok deal")
+                print("You got", percent_off, "percent off, you got an ok deal.")
         except ValueError:
-            print("You did not enter a whole number")
+            print("You did not enter a whole number.")
+
+
+def change():
+    user_input = True
+    sum = 0
+    print("Enter 'x' when you're done entering prices.")
+    while user_input:
+        #Get the price of the first item
+        price = float(input("Enter the price of an item.\n"))
+        sum += price
+        if price == "x" or price == "X":
+            user_input = False
+    #Get the payment amount
+    payment = float(input("How much did you pay?\n"))
+
+    difference = payment - sum
+    # Determine if customer still owes
+    if payment < sum:
+        print("You still owe ${:,.2f}".format(-difference))
+
+    # or if change is owed
+    if payment>sum:
+        print("Thank you, your change is ${:,.2f}".format(difference))
+    else:
+        print("Thank you for shopping with us, and have a great day.")
