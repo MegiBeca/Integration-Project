7fc97beff912fa4918006c2b962a8e7a40d00d27
MegiBeca <54412551+MegiBeca@users.noreply.github.com>
2019-11-21 21:50:18 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,19 +9,20 @@ import time
 
 end = True
 while end:
-    request = input("Pick a letter:\nA = Identify thhe persecnt yoy saved.\nB = Identify what level of savings you got.\n"
-                    "C = Identify how much you owe or how much change you are owed.\nD = Format a receipt.\nE = Calculate how "
-                    "much of a bonus you reseaved this year.\nF = Calculate your yearly income.\n").upper()
-    if(request == "A"):
+    request = input(
+        "Pick a letter:\nA = Identify thhe persecnt yoy saved.\nB = Identify what level of savings you got.\n"
+        "C = Identify how much you owe or how much change you are owed.\nD = Format a receipt.\nE = Calculate how "
+        "much of a bonus you reseaved this year.\nF = Calculate your yearly income.\n").upper()
+    if request == "A":
         Sales.identifying_percent_off()
-    elif(request == "B"):
+    elif request == "B":
         Sales.identifying_savings()
-    elif(request == "C"):
+    elif request == "C":
         Sales.change()
-    elif(request == "D"):
+    elif request == "D":
         Formatter.formatting_receipts()
-    elif(request == "E"):
+    elif request == "E":
         Pay.bonuses()
-    elif(request == "F"):
+    elif request == "F":
         Pay.income()
     time.sleep(5)
